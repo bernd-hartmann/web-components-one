d0fa19c4c7cf416e93a20599d5f65fef5e55798f
bernd-hartmann <bernd.hartmann86@t-online.de>
2018-12-16 18:26:26 +0100

    added example code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-momentum-element',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Native
 })
+
 export class AppComponent {
-  title = 'web-components-one';
+  @Input() name = 'friend';
+  focus: string;
+  focusSet = false;
+
+  setFocus(value) {
+    this.focus = value;
+    this.focusSet = true;
+  }
 }
